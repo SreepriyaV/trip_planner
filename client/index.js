642209939f63d62afcd45b80373baf1830782482
sreepriya <sreepriyav@live.com>
2017-10-03 18:02:59 -0400

    sarting off

---
+++ client/index.js
@@ -0,0 +1,14 @@
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker.js");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZ3JhY2Vob3BwZXIxNzA5IiwiYSI6ImNqOGJxcjF0ajAwdmcycXA3czU1OTA0ODUifQ.B9FFZRTrhXtxIFuFuXqZQQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack coordinates
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker("activities", [-74.009, 40.705]);
+marker.addTo(map);
---
+++ server/models/index.js
@@ -0,0 +1,79 @@
+var Sequelize = require('sequelize');
+
+var db = new Sequelize('postgres://localhost:5432/tripplanner', {
+    logging: false
+});
+
+var Hotel= db.define('hotel',{
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    num_stars:{
+        type: Sequelize.FLOAT(1,5)
+    },
+    amenities:{
+        type:Sequelize.STRING,
+        get: function()
+        {
+            return this.getDataValues('amenities').join(', ');
+        }
+    }
+   
+
+})
+
+
+var Activity=db.define('activity',{
+    name: {
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    age_range:{
+        type: Sequelize.STRING
+    }
+})
+
+var Restaurant=db.define('restaurant',{
+    
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    cuisine:{
+        type:Sequelize.STRING,
+        get: function()
+        {
+            return this.getDataValues('cuisine').join(', ');
+        }
+    },
+    price:{
+        type: Sequelize.INTEGER
+    }
+})
+
+var Place=db.define('place',{
+    address:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    city:{
+        type:Sequelize.STRING
+    },
+    state:{
+        type:Sequelize.STRING
+    },
+    phone:{
+        type:Sequelize.STRING
+    },
+    location:{
+        type: Sequelize.ARRAY(Sequelize.FLOAT)
+    }
+})
+
+Hotel.belongsTo(Place);
+Restaurant.belongsTo(Place);
+Activity.belongsTo(Place);
+
+module.exports={Place,Activity,Hotel,Restaurant,db};
+
